4b6d50496b20fe821a5530e0192794e6e4f22ec1
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-05 19:52:49 -0300

    feat: create sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,22 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { message: 'I have signin' };
+  async signin(dto: AuthDto) {
+    // find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    // compare password
+    const passwordMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!passwordMatches) throw new ForbiddenException('Credentials incorrect');
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
