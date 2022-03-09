cc0968f5ddf164e28f1918ca43641dd4c8d40469
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-09 18:26:50 -0300

    feat: create custom params decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
+
+  @Patch()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  editUser() {}
 }
